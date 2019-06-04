0a876f9fee25b6b15b04ff1c62e86e7dd2f74ce8
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-06-04 03:29:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,14 +17,17 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  []
+  ["1","2","3"]
+  remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ["1","2","3"]
+  remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+  ["1","2","3"]
   
 end
 
