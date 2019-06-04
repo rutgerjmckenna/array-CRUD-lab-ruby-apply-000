fd295f39fa53631b75ced91aabdce63a81222e8b
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-06-04 03:33:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  ["1","2","3"]
+  ["wow", "I", "am", "really", "learning", "arrays!"]
   array.first
 end
 
