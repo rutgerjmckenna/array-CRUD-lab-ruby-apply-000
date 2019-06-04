babb109b801fc13c29a4f5f09696f04d2165332b
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-06-04 03:31:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,32 +8,32 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["1","2"]
-  add_element_to_end_of_array.push(3)
+  array.push(3)
 end
 
 def add_element_to_start_of_array(array, element)
   ["1","2"]
-  add_element_to_start_of_array.unshift(0)
+  array.unshift(0)
 end
 
 def remove_element_from_end_of_array(array)
   ["1","2","3"]
-  remove_element_from_end_of_array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
   ["1","2","3"]
-  remove_element_from_start_of_array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
   ["1","2","3"]
-  retrieve_element_from_index[1]
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
   ["1","2","3"]
-  retrieve_first_element_from_array.first
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
